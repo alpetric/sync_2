f58f5e57f671e5e48098e01a522b780faed34c51
alex <alex@windmill.dev>
2025-06-26 20:06:54 +0000

    [WM] Script 'f/test/willing_script' deployed

--- f/test/willing_script.ts
+++ f/test/willing_script.ts
@@ -1,4 +1,4 @@
-// there are multiple modes to add as header: //nobundling //native //npm //nodejsadsadsfadfafdadfadf
+// there are multiple modes to add as header: //nobundling //native //npm //nodejsadsadsfadfafdadfadfadsf
 // https://www.windmill.dev/docs/getting_started/scripts_quickstart/typescript#modes
 
 // import { toWords } from "number-to-words@1"adfadsf
