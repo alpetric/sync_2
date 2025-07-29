b479f77c1247c2cbf5c7e086bb01081e0ff32f32
alex@windmill.dev <alex@windmill.dev>
2025-07-29 19:48:50 +0000

    [WM] Script 'f/test/tops_script' deployed

--- test22/f/test/tops_script.ts
+++ test22/f/test/tops_script.ts
@@ -1,4 +1,4 @@
-// https://www.windmill.dev/docs/getting_started/scripts_quickstart/typescript#modes
+// https://www.windmill.dev/docs/getting_started/scripts_quickstart/typescript#modesadfadsf
 
 // import { toWords } from "number-to-words@1"
 import * as wmill from "windmill-client"
