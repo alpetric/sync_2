d04b370b235587803e2ddcb73f09e4ae926b31a5
alex <alex@windmill.dev>
2025-08-26 09:32:55 +0000

    [WM] Script 'u/alex/hilarious_script' deployed

--- test22/u/alex/hilarious_script.ts
+++ test22/u/alex/hilarious_script.ts
@@ -1,5 +1,14 @@
-export async function main(raw_email: string, parsed_email: any) {
-  // do something with the email
-  console.log(raw_email)
-  console.log(parsed_email)
-}
+ export async function main(raw_email: string, parsed_email: any, ...extraArgs: any[]) {
+    console.log("Raw email:", raw_email);
+    console.log("Parsed email:", parsed_email);
+    console.log("Extra args:", extraArgs);
+
+    // The extra args will be passed in the order they were parsed
+    // You can also access them from the Windmill job context if available
+
+    return {
+      raw_email_length: raw_email.length,
+      parsed_subject: parsed_email.headers?.Subject,
+      extra_args_count: extraArgs.length
+    };
+  }
