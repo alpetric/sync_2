86dc01d907dc2cb44a506798f7c3ceba44e90aa6
alex <alex@windmill.dev>
2025-11-03 16:51:15 +0000

    [WM] Script 'u/alex/industrious_script' deployed

--- test22/u/alex/industrious_script.ts
+++ test22/u/alex/industrious_script.ts
@@ -1,5 +1,5 @@
 // import * as wmill from "windmill-client"
-import wmill from "windmill-cli@1.542.1";
+import wmill from "windmill-cli@1.541.1";
 
 export async function main() {
   await wmill.parse([
